67e69c6bb370aa0a1465760c269564211f963e7c
Kris Gautam <92295923+Kris248@users.noreply.github.com>
2025-05-21 11:29:35 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,30 +2,22 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        // Creating object of Product class
         Product p = new Product();
-
-        // Calling overloaded methods
-        System.out.println(p.product(5, 10));        // Calls product(int, int)
-        System.out.println(p.product(2, 3, 4));      // Calls product(int, int, int)
-        System.out.println(p.product(2.5, 4.5));     // Calls product(double, double)
+        System.out.println(p.product(5, 10));
+        System.out.println(p.product(2, 3, 4));
+        System.out.println(p.product(2.5, 4.5));
     }
 }
 
-// Task 1: Creating Product class inside Main class
 class Product {
-
-    // Task 3: Method with two int parameters
     public int product(int x, int y) {
         return x * y;
     }
 
-    // Task 4: Overloaded method with three int parameters
     public int product(int x, int y, int z) {
         return x * y * z;
     }
 
-    // Task 5: Overloaded method with two double parameters
     public double product(double x, double y) {
         return x * y;
     }
